ed9c43f85f8d24cf2671fc65d8551da6940ea341
Augustine Akoto Larbi-Ampofo <augustine.larbi-ampofo@persol.net>
2021-09-08 14:37:14 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import NavBar from './components/NavBar'
+import Products from './components/Products'
+import { useState } from 'react'
 
 function App() {
+  const [products, setProducts] = useState([
+    { id: 'p0001', name: 'Morphine', latestPrice: '100', prevPrice: '110' },
+    { id: 'p0002', name: 'Ibuprufen', latestPrice: '120', prevPrice: '140' },
+    { id: 'p0003', name: 'Morphine', latestPrice: '100', prevPrice: '110' },
+    { id: 'p0004', name: 'Ibuprufen', latestPrice: '120', prevPrice: '140' },
+    { id: 'p0005', name: 'Morphine', latestPrice: '100', prevPrice: '110' },
+    { id: 'p0006', name: 'Ibuprufen', latestPrice: '120', prevPrice: '140' },
+    { id: 'p0007', name: 'Morphine', latestPrice: '100', prevPrice: '110' },
+    { id: 'p0008', name: 'Ibuprufen', latestPrice: '120', prevPrice: '140' },
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <br />
+      <NavBar />
+      <Products products={products} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Products.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import ProductItem from './ProductItem'
+import { makeStyles } from '@material-ui/core/styles'
+import { DataGrid } from '@material-ui/data-grid'
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: 600,
+  },
+})
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 100 },
+  {
+    field: 'name',
+    headerName: 'Product Name',
+    width: 200,
+    editable: false,
+  },
+  {
+    field: 'latestPrice',
+    headerName: 'Price (GHS)',
+    type: 'number',
+    width: 250,
+    editable: true,
+  },
+  {
+    field: 'prevPrice',
+    headerName: 'Previous Price (GHS)',
+    type: 'number',
+    width: 250,
+    editable: false,
+  },
+  { field: 'action', headerName: 'Action', width: 150 },
+]
+
+function Products({ products }) {
+  const classes = useStyles()
+  const [page, setPage] = React.useState(0)
+  const [rowsPerPage, setRowsPerPage] = React.useState(10)
+
+  return (
+    <div style={{ margin: '0 auto', height: 600, width: '50%' }}>
+      <DataGrid
+        rows={products}
+        columns={columns}
+        pageSize={10}
+        checkboxSelection
+        //disableSelectionOnClick
+      />
+    </div>
+  )
+}
+
+export default Products
